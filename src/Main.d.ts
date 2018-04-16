12c9dfef93260e7f788446ee68648ce7914033b2
Harm Boschloo <harm@boschloo.net>
2018-04-16 22:54:53 +0200

    type def

--- src/Main.d.ts
+++ src/Main.d.ts
@@ -4,5 +4,5 @@ export interface App {}
 
 export namespace Main {
   export function fullscreen(): App;
-  export function embed(node: HTMLElement | null): App;
+  export function embed(element: HTMLElement): App;
 }
